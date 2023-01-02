dd6305dfe37c4a9bb8302013d25cb00def29e327
Robin MORET <robin.moret@supaero.org>
2023-01-02 11:28:36 +0100

    Released 13.4.0

--- main.inc.php
+++ main.inc.php
@@ -23,7 +23,7 @@
 
 /*
 Plugin Name: LocalFiles Editor
-Version: 12.3.0
+Version: 13.4.0
 Description: Edit local files from administration panel
 Plugin URI: http://piwigo.org/ext/extension_view.php?eid=144
 Author: Piwigo team
